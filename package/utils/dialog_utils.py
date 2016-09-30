cae51baacc63a5fd2f23546e2f18a4ea422ca024
joewledger <joewledger@gmail.com>
2016-09-29 20:00:33 -0400

    Added documentation to the utils modules
    Signed-off-by: joewledger <joewledger@gmail.com>

--- package/utils/dialog_utils.py
+++ package/utils/dialog_utils.py
@@ -1,23 +1,31 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+
+#Opens a file dialog and gets the filepath selected by the user
 def fileDialog():
-	fileDialog = QFileDialog()
-	file = fileDialog.getOpenFileName()
-	return file[0]
+    fileDialog = QFileDialog()
+    file = fileDialog.getOpenFileName()
+    return file[0]
 
+
+#Opens a directory dialog and gets the directory selected by the user
 def directoryDialog():
-	dialog = QFileDialog()
-	directory = dialog.getExistingDirectory()
-	return directory
+    dialog = QFileDialog()
+    directory = dialog.getExistingDirectory()
+    return directory
 
-def messageDialog(window_title,message):
+
+#Opens a message dialog with the provided message in a separate window
+def messageDialog(window_title, message):
     msg = QMessageBox()
     msg.setWindowTitle(window_title)
     msg.setText(message)
     msg.exec_()
 
+
+#Opens a color dialog and gets the color selected by the user
 def colorDialog():
-	dialog = QColorDialog()
-	color = dialog.getColor()
-	return color
+    dialog = QColorDialog()
+    color = dialog.getColor()
+    return color
--- package/utils/param_utils.py
+++ package/utils/param_utils.py
@@ -1,33 +1,52 @@
 from copy import *
 
-#Takes a dictionary mapping Parameter Expectation objects to their default values
+
+"""
+The classes in this module are designed to allow plotters to specify what types of parameters they
+want to request from the Graph Edit Widget, as well as to store the default values of these parameters.
+
+For example, the Light_Occupancy_Pie_Chart_Plotter requests a title parameter (which is a string),
+and a colors parameter (which is a list of 4 colors).
+
+"""
+
+
 class Parameter_Collection():
 
-    def __init__(self,param_dictionary):
+    #Initializes the parameter collection, using a dictionary that maps parameter expectations -> default values
+    def __init__(self, param_dictionary):
         self.param_dict = param_dictionary
         self.expectation_list = list(self.param_dict.keys())
         self.current = 0
         self.high = len(self.expectation_list)
 
+    #Returns a dictionary mapping parameter expectation names to the current value of that parameter expectation
     def get_param_name_to_value_dict(self):
-        return {x.param_name : self.param_dict[x] for x in self.param_dict.keys()}
+        return {x.param_name: self.param_dict[x] for x in self.param_dict.keys()}
 
+    #Returns a list of all the names of the parameter expectations
     def unpack_names(self):
         return [x.param_name for x in self.param_dict.keys()]
 
+    #Returns a list of all the current values
     def unpack_values(self):
         return [x for x in self.param_dict.values()]
 
-    #param_dictionary can be either a dictionary or an ordered_dictionary
-    def update_values(self,param_dict):
+    #Updates the values of this parameter collection through the use of a dictionary
+    #that maps parameter expectations -> new values
+    #param_dict can be either a dictionary or an ordered_dictionary
+    def update_values(self, param_dict):
         for key in param_dict.keys():
             self.param_dict[key] = param_dict[key]
 
-    def update_values_param_collection(self,parameter_collection):
+    #Updates the values of this parameter collection through the use of another parameter_collection
+    #
+    def update_values_param_collection(self, parameter_collection):
         for key in parameter_collection.param_dict.keys():
             self.param_dict[key] = parameter_collection[key]
 
-    def __getitem__(self,key):
+    #Gets default value for a given parameter expectation
+    def __getitem__(self, key):
         return self.param_dict[key]
 
     def __len__(self):
@@ -53,15 +72,16 @@ class Parameter_Collection():
         return new
 
 
+#The Parameter Expectation class contains a the parameter name, and the type of parameter
 class Parameter_Expectation():
 
-    def __init__(self,param_name,param_type_wrapper):
+    def __init__(self, param_name, param_type_wrapper):
         self.param_name = param_name
         self.param_type_wrapper = param_type_wrapper
 
-    def copy_new_list_length(self,n):
+    def copy_new_list_length(self, n):
         try:
-            return Parameter_Expectation(self.param_name,self.param_type_wrapper.copy_new_list_length(n))
+            return Parameter_Expectation(self.param_name, self.param_type_wrapper.copy_new_list_length(n))
         except:
             return self
 
@@ -77,12 +97,16 @@ class Parameter_Expectation():
     def __len__(self):
         return self.param_type_wrapper.length
 
+
+#This class is used to 'wrap' the type of a given parameter
+#For instance, if a given plotter is expecting 4 colors, a parameter expectation could be initialized as follows:
+#param_utils.Parameter_Expectation("Color", param_utils.Param_Type_Wrapper(type(QColor()), is_list=True, length=4))
 class Param_Type_Wrapper():
 
-    def __init__(self,_type,is_list=False,length=1):
+    def __init__(self, _type, is_list=False, length=1):
         self._type = _type
         self.is_list = is_list
         self.length = length
 
-    def copy_new_list_length(self,n):
-        return Param_Type_Wrapper(self._type, is_list=True,length=n)
+    def copy_new_list_length(self, n):
+        return Param_Type_Wrapper(self._type, is_list=True, length=n)
